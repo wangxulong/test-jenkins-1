c905f685fd3f94a5d9acb627f287863693c4de53
wangxulong <duanxian0402@126.com>
2020-02-07 18:12:44 +0800

    打包

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ action=$1
 # 打包
 dev_package(){
    echo "开始打包"
-    docker run --rm -w /app -v $HOME/.m2:/root/.m2 -v `pwd`:/app \
+   docker run --rm -w /app -v $HOME/.m2:/root/.m2 -v `pwd`:/app \
         maven:3.6.1-jdk-11-slim \
         mvn clean package
-     echo "打包结束"
-   mvn clean package
+   echo "打包结束"
+
 }
 
 if [[ ${action} == "package" ]]; then
