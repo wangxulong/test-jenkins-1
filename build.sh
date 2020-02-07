ca9ee306c6e8a5d359bd7caefae7e225e1a6195c
wangxulong <duanxian0402@126.com>
2020-02-07 18:12:07 +0800

    添加build

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # 设置环境变量
-echo $JENKINS_HOME
-echo `pwd`
-export PATH=$JENKINS_HOME/maven/bin:$PATH
 # 参数
 action=$1
 # 打包
 dev_package(){
+   echo "开始打包"
+    docker run --rm -w /app -v $HOME/.m2:/root/.m2 -v `pwd`:/app \
+        maven:3.6.1-jdk-11-slim \
+        mvn clean package
+     echo "打包结束"
    mvn clean package
 }
 
 if [[ ${action} == "package" ]]; then
     dev_package
 fi
-
-dev_package
