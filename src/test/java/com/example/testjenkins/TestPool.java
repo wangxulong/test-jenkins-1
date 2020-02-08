4fded2d5c17673806adc12d3a1cacab4ebae163f
wangxulong <duanxian0402@126.com>
2020-02-08 10:41:12 +0800

    集成jacoco

--- src/test/java/com/example/testjenkins/TestPool.java
+++ src/test/java/com/example/testjenkins/TestPool.java
@@ -1,12 +1,10 @@
 package com.example.testjenkins;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.ArrayList;
 import java.util.List;
